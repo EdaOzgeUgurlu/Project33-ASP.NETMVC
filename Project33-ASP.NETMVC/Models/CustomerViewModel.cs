e20dcb3ecf503429165aa913a99a58bc9f14e7f5
EdaOzge <eozge.ugurlu@tedu.edu.tr>
2024-11-20 22:14:46 +0300

    Proje dosyası ekle.

---
+++ Project33-ASP.NETMVC/Models/CustomerViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Project33_ASP.NETMVC.Models
+{
+    public class CustomerViewModel
+    {
+        public Customer Customer { get; set; }
+        public string WelcomeMessage { get; set; }
+
+    }
+}
